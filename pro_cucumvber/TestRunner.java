cbaba73ec5ac9c16db962675fac5d4f6ca6ebbe0
root <root@0dd9458e3f9d>
2025-05-20 11:52:30 +0530

    Pushed Successfully

---
+++ pro_cucumvber/TestRunner.java
@@ -0,0 +1,15 @@
+package runners;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/java/features",
+    glue = "stepdefinitions",
+    plugin = {"pretty"},
+    monochrome = true
+)
+public class TestRunner {
+}
